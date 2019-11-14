2c30c72c5c745b260df17000527a77252532f8d9
Carl Barringer <cbarringer1@gmail.com>
2019-11-14 13:10:13 -0600

    Students is Complete and working

--- TESTQuery.sql
+++ TESTQuery.sql
@@ -1,4 +1,7 @@
 ﻿INSERT INTO Student (FirstName, LastName, SlackHandle, CohortId) VALUES ('Tom', 'Smith', '@Tommy', '1');
 
+INSERT INTO Instructor (FirstName, LastName, SlackHandle, CohortId, Specialty) VALUES ('Bryan', 'Nilsen', '@Bryan', '1', 'Instructing');
+
+
 select * from cohort
 
